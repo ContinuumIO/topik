b3e107a2b6a3842736df3792f9a8bdeb294569d3
Christine Doig <christine.doig.cardet@gmail.com>
2015-02-13 09:57:33 -0600

    add setup  with entrypoint

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ from setuptools import setup, find_packages
 
 setup(name='topik',
       version='0.1.0',
-      description='A Topic Modeling high-level interface',
+      description='A Topic Modeling toolkit',
       url='http://github.com/ContinuumIO/topik/',
       author='Christine Doig',
       author_email='christine.doig@continuum.io',
       license='BSD',
-      keywords='topic modeling lda nltk gensim',
+      keywords='topic modeling lda nltk gensim pattern',
       packages=find_packages(),
       install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      entry_points= {
+          'console_scripts': ['topik = topik.cli:run']
+      },
       long_description=(open('README.md').read() if exists('README.md')
                         else ''), zip_safe=False)
