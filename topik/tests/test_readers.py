4f50eace339fb20db537bcc241421f793a7f6a6a
David Mertz <dmertz@continuum.io>
2015-06-09 14:39:16 -0700

    Fix test_readers.py glitches

--- topik/tests/test_readers.py
+++ topik/tests/test_readers.py
@@ -10,28 +10,26 @@ class TestReader(unittest.TestCase):
                                         The plot is definitely one of the most original I've seen in a while."
 
     def test_iter_document_json_stream(self):
-        id_documents = iter_document_json_stream(
+        id_texts = iter_document_json_stream(
                 './topik/tests/data/test-data-1.json', "text")
-        ids, doc_text = unzip(id_docuents)
-        first_document = next(doc_text)
-        self.assertTrue(first_document, self.solution)
+        ids, texts = unzip(id_texts)
+        first_text = next(texts)
+        self.assertTrue(first_text, self.solution)
 
     def test_iter_documents_folder(self):
-        id_documents = iter_documents_folder(
+        id_texts = iter_documents_folder(
                 './topik/tests/data/test-data-folder')
-        ids, doc_text = unzip(id_documents)
-        for fname, document in doc_text:
+        for fname, text in id_texts:
             if fname=='doc1':
-                self.assertTrue(document, self.solution)
+                self.assertTrue(text, self.solution)
                 break
 
     def test_iter_documents_folder_gz(self):
-        id_documents = iter_documents_folder(
+        id_texts = iter_documents_folder(
                 './topik/tests/data/test-data-folder-gz')
-        ids, doc_text = unzip(id_documents)
-        for fname, document in doc_text:
+        for fname, text in id_texts:
             if fname=='doc1.gz':
-                self.assertTrue(document, self.solution)
+                self.assertTrue(text, self.solution)
                 break
 
 if __name__ == '__main__':
