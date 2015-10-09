af885fc88b18e04bfcbca4c64cbe127fff95d580
Reed Youngblood <ryoungblood@continuum.io>
2015-10-09 14:44:18 -0500

    removed to_r_ldavis from topik.utils

--- topik/utils.py
+++ topik/utils.py
@@ -10,40 +10,6 @@ import pandas as pd
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-
-def to_r_ldavis(corpus_bow, lda, dir_name):
-    """Generate the input that the R package LDAvis needs.
-
-    Parameters
-    ----------
-    corpus_bow : topik.intermediares.digested_document_collection.DigestedDocumentCollection instance
-        The corpus bag-of-words representation
-    lda : topik.models.LDA instance
-    dir_name : string
-        Directory name where to store the files required in R package LDAvis
-
-    """
-
-    if not os.path.isfile(dir_name):
-        os.makedirs(dir_name)
-
-    corpus_bow.dict.save_as_text(os.path.join(dir_name, 'dictionary'), sort_by_word=False)
-
-    df = pd.read_csv(os.path.join(dir_name, 'dictionary'), sep='\t', index_col=0, header=None)
-    df = df.sort_index()
-
-    df[1].to_csv(os.path.join(dir_name, 'vocab'), index=False, header=False)
-    df[2].to_csv(os.path.join(dir_name, 'term_frequency'), index=False)
-
-    tt_dist = np.array(lda.model.expElogbeta)
-    np.savetxt(os.path.join(dir_name, 'topicTermDist'), tt_dist, delimiter=',', newline='\n',)
-
-    corpus_file = corpus_bow.filename
-    corpus = gensim.corpora.MmCorpus(corpus_file)
-    docTopicProbMat = lda.model[corpus]
-    gensim.corpora.MmCorpus.serialize(os.path.join(dir_name, 'docTopicProbMat.mm'), docTopicProbMat)
-
-
 def generate_csv_output_file(reader, tokenizer, corpus_bow, lda_model, output_file='output.csv'):
     """Utility function to retrieve the results of your topic modeling in a csv file.
     Uses a pandas.DataFrame, only useful for smaller datasets.
