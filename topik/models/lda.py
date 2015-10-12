66c6df5f826cfd274200ba913bff0e2ae80a58d2
Reed Youngblood <ryoungblood@continuum.io>
2015-10-12 12:23:52 -0500

    fixed pyLDAvis ValidationError
    fixes #34

--- topik/models/lda.py
+++ topik/models/lda.py
@@ -47,7 +47,8 @@ class LDA(TopicModelBase):
     def __init__(self, corpus_input=None, ntopics=10, load_filename=None, binary_filename=None, **kwargs):
         if corpus_input is not None:
             self._model = gensim.models.LdaModel(list(iter(corpus_input)), num_topics=ntopics,
-                                                id2word=corpus_input.get_id2word_dict(), **kwargs)
+                                                 id2word=corpus_input.get_id2word_dict(),
+                                                 minimum_probability=0, **kwargs)
             self._corpus = corpus_input
         elif load_filename is not None and binary_filename is not None:
             self._model = gensim.models.LdaModel.load(binary_filename)
