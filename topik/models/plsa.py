fix PLSA normalization.  move tests for data input to plotting

topik/models/plsa.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import itertools
 import logging
-import math
-import random
 
 import numpy as np
-import pandas as pd
 
 from .base_model_output import ModelOutput
 from ._registry import register
@@ -16,79 +12,55 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.WARNING)
 
 
-def _rand_mat(sizex, sizey):
-    ret = []
-    for i in xrange(sizex):
-        ret.append([])
-        for _ in xrange(sizey):
-            ret[-1].append(random.random())
-        norm = sum(ret[-1])
-        for j in xrange(sizey):
-            ret[-1][j] /= norm
-    return ret
+def _rand_mat(rows, cols):
+    out = np.random.random((rows, cols))
+    for row in out:
+        row /= row.sum()
+    return out
 
 
-def _cal_p_dw(vectors, topic_array, zw, dz, beta, p_dw):
-    for d, (doc_id, doc) in enumerate(vectors):
-        for word_id, word_ct in doc.items():
-            tmp = 0
-            for _ in range(word_ct):
-                for z in topic_array:
-                    tmp += (zw[z][word_id]*dz[d][z])**beta
-            p_dw[d][word_id] = tmp
+def _cal_p_dw(words_in_docs, word_cts_in_docs, topic_array, zw, dz, beta, p_dw):
+    for d, (doc_id, words) in enumerate(words_in_docs.items()):
+        for z in topic_array:
+            p_dw[d, words] = word_cts_in_docs[doc_id] * (zw[z][words]*dz[d][z])**beta
     return p_dw
 
 
-def _e_step(vectors, dw_z, topic_array, zw, dz, beta, p_dw):
-    for d, (doc_id, doc) in enumerate(vectors):
-        for word_id, word_ct in doc.items():
-            dw_z[d][word_id] = []
-            for z in topic_array:
-                dw_z[d][word_id].append(((zw[z][word_id]*dz[d][z])**beta)/p_dw[d][word_id])
+def _e_step(words_in_docs, dw_z, topic_array, zw, dz, beta, p_dw):
+    for d, words in enumerate(words_in_docs.values()):
+        for z in topic_array:
+            dw_z[d, words, z] = ((zw[z, words] * dz[d, z]) ** beta) / p_dw[d, words]
     return dw_z
 
 
-def _m_step(vectorized_corpus, topic_array, zw, dw_z, dz):
-    #iterators = itertools.tee(vectorized_corpus, len(topic_array)+1)
+def _m_step(words_in_docs, word_cts_in_docs, topic_array, zw, dw_z, dz):
     for z in topic_array:
-        zw[z] = [0, ] * vectorized_corpus.global_term_count
-        for d, (doc_id, doc) in enumerate(vectorized_corpus.get_vectors()):
-            for word_id, word_ct in doc.items():
-                zw[z][word_id] += word_ct*dw_z[d][word_id][z]
+        zw[z] = 0
+        for d, (doc_id, words) in enumerate(words_in_docs.items()):
+            zw[z, words] += word_cts_in_docs[doc_id]*dw_z[d, words, z]
     # normalize by sum of topic word weights
     for topic in zw:
-        total = sum(topic)
-        for term in topic:
-            if total == 0.0:
-                print('pause here')
-                print(term)
-                print(topic)
-                print(total)
-            term /= total
-    for d, (doc_id, doc) in enumerate(vectorized_corpus.get_vectors()):
-        dz[d] = [0, ] * len(topic_array)
+        topic /= sum(topic)
+    for d, (doc_id, words) in enumerate(words_in_docs.items()):
         for z in topic_array:
-            for word_id, word_ct in doc.items():
-                dz[d][z] += word_ct * dw_z[d][word_id][z]
-        for z in topic_array:
-            dz[d][z] /= vectorized_corpus.document_term_counts[doc_id]
+            dz[d, z] = sum(word_cts_in_docs[doc_id] * dw_z[d, words, z])
+        dz[d] /= sum(dz[d])
     return zw, dz
 
 
-def _cal_likelihood(vectors, p_dw):
+def _cal_likelihood(words_in_docs, word_cts_in_docs, p_dw):
     likelihood = 0
-    for d, (doc_id, doc) in enumerate(vectors):
-        for word_id, word_ct in doc.items():
-            likelihood += word_ct*math.log(p_dw[d][word_id])
+    for d, (doc_id, words) in enumerate(words_in_docs.items()):
+        likelihood += sum(word_cts_in_docs[doc_id] * np.log(p_dw[d][words]))
     return likelihood
 
-def _get_topic_term_matrix(zw, ntopics, id_term_map):
 
+def _get_topic_term_matrix(zw, ntopics, id_term_map):
     labeled_zw = {"topic"+str(topicno): zw[topicno] for topicno in range(ntopics)}
     return labeled_zw
 
-def _get_doc_topic_matrix(dz, ntopics, vectorized_corpus):
 
+def _get_doc_topic_matrix(dz, ntopics, vectorized_corpus):
     labeled_dz = {doc_id: dz[i] for i, (doc_id, vector) in enumerate(vectorized_corpus.get_vectors())}
     return labeled_dz
 
@@ -100,25 +72,21 @@ def _PLSA(vectorized_corpus, ntopics=3, max_iter=100):
     zw = _rand_mat(ntopics, vectorized_corpus.global_term_count)
     # document-topic matrix
     dz = _rand_mat(len(vectorized_corpus), ntopics)
-    dw_z = [{}, ] * len(vectorized_corpus)
-    p_dw = [{}, ] * len(vectorized_corpus)
+    dw_z = np.zeros((len(vectorized_corpus), vectorized_corpus.global_term_count, ntopics))
+    p_dw = np.zeros((len(vectorized_corpus), vectorized_corpus.global_term_count))
     beta = 0.8
+    words_in_docs = {doc_id: [word_id for word_id, _ in doc.items()] for doc_id, doc in vectorized_corpus.get_vectors()}
+    word_cts_in_docs = {doc_id: [ct for _, ct in doc.items()] for doc_id, doc in vectorized_corpus.get_vectors()}
     for i in range(max_iter):
-        #iter1, iter2, iter3 = itertools.tee(vectorized_corpus, 3)
-        #logging.info('%d iter' % i)
-        vectorized_corpus.get_vectors()
-        p_dw = _cal_p_dw(vectorized_corpus.get_vectors(), topic_array, zw, dz,
-                         beta, p_dw)
-        dw_z = _e_step(vectorized_corpus.get_vectors(), dw_z, topic_array, zw,
-                       dz, beta, p_dw)
-        zw, dz = _m_step(vectorized_corpus, topic_array, zw, dw_z, dz)
-        likelihood = _cal_likelihood(vectorized_corpus.get_vectors(), p_dw)
-        #logging.info('likelihood %f ' % likelihood)
+        p_dw = _cal_p_dw(words_in_docs, word_cts_in_docs, topic_array, zw, dz, beta, p_dw)
+        dw_z = _e_step(words_in_docs, dw_z, topic_array, zw, dz, beta, p_dw)
+        zw, dz = _m_step(words_in_docs, word_cts_in_docs, topic_array, zw, dw_z, dz)
+        likelihood = _cal_likelihood(words_in_docs, word_cts_in_docs, p_dw)
         if cur != 0 and abs((likelihood-cur)/cur) < 1e-8:
             break
         cur = likelihood
     return _get_topic_term_matrix(zw, ntopics, vectorized_corpus.id_term_map), \
-            _get_doc_topic_matrix(dz, ntopics, vectorized_corpus)
+        _get_doc_topic_matrix(dz, ntopics, vectorized_corpus)
 
 @register
 def plsa(vectorized_corpus, **kwargs):

topik/models/tests/test_plsa.py
@@ -1,14 +1,16 @@
 import random
 
 import nose.tools as nt
+import numpy as np
 
-from topik.models import registered_models, PLSA
+from topik.models import registered_models
 from .test_data import test_vectorized_output
 
-from topik.models.plsa import _rand_mat, _e_step, _m_step, _cal_likelihood, _cal_p_dw
+from topik.models.plsa import _rand_mat, _e_step, _m_step, _cal_likelihood, _cal_p_dw, plsa
 
 ntopics = 2
-
+words_in_docs = {doc_id: [word_id for word_id, _ in doc.items()] for doc_id, doc in test_vectorized_output.get_vectors()}
+word_cts_in_docs = {doc_id: [ct for _, ct in doc.items()] for doc_id, doc in test_vectorized_output.get_vectors()}
 
 def test_rand_mat():
     # ntopics, nwords
@@ -18,39 +20,38 @@ def test_rand_mat():
         nt.assert_equal(len(topic), 5)
         nt.assert_almost_equal(sum(topic), 1)
 
-def test_e_step():
-    dz = _rand_mat(len(test_vectorized_output), ntopics)
-    dw_z = [{}, ] * len(test_vectorized_output)
-    p_dw = [{}, ] * len(test_vectorized_output)
-    zw = _rand_mat(ntopics, test_vectorized_output.global_term_count)
-    dw_z = _e_step(test_vectorized_output, dw_z, range(ntopics), zw, dz, 0.8, p_dw)
-    nt.assert_almost_equal(dw_z, 1)
 
-def test_m_step():
+def test_em():
     dz = _rand_mat(len(test_vectorized_output), ntopics)
-    dw_z = [{}, ] * len(test_vectorized_output)
+    dw_z = np.zeros((len(test_vectorized_output), test_vectorized_output.global_term_count, ntopics))
+    p_dw = np.zeros((len(test_vectorized_output), test_vectorized_output.global_term_count))
     zw = _rand_mat(ntopics, test_vectorized_output.global_term_count)
-    zw, dz = _m_step(test_vectorized_output, range(ntopics), zw, dw_z, dz)
+    p_dw = _cal_p_dw(words_in_docs, word_cts_in_docs, range(ntopics), zw, dz, 0.8, p_dw)
+    dw_z = _e_step(words_in_docs, dw_z, range(ntopics), zw, dz, 0.8, p_dw)
+    zw, dz = _m_step(words_in_docs, word_cts_in_docs, range(ntopics), zw, dw_z, dz)
     for topic in zw:
         nt.assert_almost_equal(sum(topic), 1)
     for doc in dz:
         nt.assert_almost_equal(sum(doc), 1)
-    raise NotImplementedError
+
 
 def test_cal_likelihood():
-    raise NotImplementedError
+    dz = _rand_mat(len(test_vectorized_output), ntopics)
+    zw = _rand_mat(ntopics, test_vectorized_output.global_term_count)
+    p_dw = np.zeros((len(test_vectorized_output), test_vectorized_output.global_term_count))
+    p_dw = _cal_p_dw(words_in_docs, word_cts_in_docs, range(ntopics), zw, dz, 0.8, p_dw)
+    likelihood = _cal_likelihood(words_in_docs, word_cts_in_docs, p_dw)
+    nt.assert_less(likelihood, 0)
 
-def test_cal_p_dw():
-    raise NotImplementedError
 
 def test_train():
     random.seed(42)
-    model_output = PLSA(test_vectorized_output, ntopics=2)
-    for doc in model_output.doc_topic_matrix:
+    model_output = plsa(test_vectorized_output, ntopics=2)
+    for doc in model_output.doc_topic_matrix.values():
         nt.assert_almost_equal(sum(doc), 1)
-    for topic in model_output.topic_term_matrix:
+    for topic in model_output.topic_term_matrix.values():
         nt.assert_almost_equal(sum(topic), 1)
 
 
 def test_registration():
-    nt.assert_true("PLSA" in registered_models)
+    nt.assert_true("plsa" in registered_models)