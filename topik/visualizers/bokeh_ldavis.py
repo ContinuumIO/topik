e26c434e7a2adf0ce9e3fe1d86f82378f3482dc3
Brittain Hard <brittainhard@gmail.com>
2016-02-09 15:30:52 -0600

    First pass at circle plotting.

--- topik/visualizers/bokeh_ldavis.py
+++ topik/visualizers/bokeh_ldavis.py
@@ -1,1 +1,24 @@
+import numpy as np
 import pandas as pd
+
+from bokeh.plotting import figure, curdoc, vplot, output_file, show
+from bokeh.embed import autoload_server, components
+from bokeh.client import push_session
+from bokeh.models import Button, ColumnDataSource
+
+
+def bokeh_lda_vis(data=None):
+    output_file("circles.html")
+
+    plot = figure()
+    plot.circle(data["x"].tolist(), data["y"].tolist(), size=10)
+
+    # curdoc().add_root(plot)
+    # session = push_session(curdoc())
+    # script = autoload_server(model=None, session_id=session.id)
+    # return script
+    show(plot)
+
+
+data = pd.read_json("sample.json")
+bokeh_lda_vis(data)
